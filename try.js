c3c5a3c2f60f70827c6b30b9057a1973881bdcb6
Kamil <kamilhidayat.890@gmail.com>
2022-12-09 11:28:25 +0700

    Update Fitur 0.1

--- try.js
+++ try.js
@@ -1,17 +1,41 @@
 let user = document.getElementById('UserName');
 let date = document.getElementById('date');
-let umur = document.getElementById('Umur')
+let umur = document.getElementById('Umur');
+let btn = document.getElementById('btn');
+var sugi = document.getElementById('Gambar3')
+var seko = document.getElementById('Gambar2')
+var bocil = document.getElementById('Gambar1')
+
+bocil.style.display = 'none'
+sugi.style.display = 'none'
+seko.style.display = 'none'
 
 function UserInput(){
     console.log(user.value);
     console.log(date.value);
     if(umur.value < 10 && umur.value > 0){
-        document.getElementById('textumur').innerText = 'Hahaha Bocil'
+        document.getElementById('textumur').innerText = 'Hahaha Bocil';
+        user.style.display = 'none';
+        date.style.display = 'none';
+        umur.style.display = 'none';
+        btn.style.display = 'none';
+        bocil.style.display = ''
+
     } else if(umur.value > 10 && umur.value < 18){
-        document.getElementById('textumur').innerText = 'Kamu adalah Pemuda'
+        document.getElementById('textumur').innerText = 'Kamu adalah Pemuda';
+        user.style.display = 'none';
+        date.style.display = 'none';
+        umur.style.display = 'none';
+        btn.style.display = 'none';
+        seko.style.display = ''
     }
     else{
-        document.getElementById('textumur').innerText = 'Tua Banget'
+        document.getElementById('textumur').innerText = 'Tua Banget';
+        user.style.display = 'none';
+        date.style.display = 'none';
+        umur.style.display = 'none';
+        btn.style.display = 'none';
+        sugi.style.display = ''
     }
     document.getElementById('body').innerText = user.value + ' ' + 'telah log in pada' + ' ' + date.value
 }
