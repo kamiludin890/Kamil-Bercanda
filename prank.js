fddb03c7211440d17618709b334076d724fab053
Kamil <kamilhidayat.890@gmail.com>
2022-12-10 10:35:18 +0700

    REFRESH FITUR

---
+++ prank.js
@@ -0,0 +1,10 @@
+const prank = document.getElementById('Prank');
+setTimeout(function (){
+    prank.style.display = 'none'
+}, 70000);
+
+if(prank.style.display = 'none'){
+    location.replace("/index.html")
+    clearTimeout(prank.style.display = 'none')
+    prank.style.display = ''
+}
